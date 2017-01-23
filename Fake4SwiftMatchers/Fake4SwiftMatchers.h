ff4f8c226b27e4e259e7a3f7075f9f468a78495d
Rachel Brindle and Tim Jarratt <rbrindle+tjarratt@pivotal.io>
2017-01-23 15:04:08 -0800

    Initial Commit

---
+++ Fake4SwiftMatchers/Fake4SwiftMatchers.h
@@ -0,0 +1,19 @@
+//
+//  Fake4SwiftMatchers.h
+//  Fake4SwiftMatchers
+//
+//  Created by Tim Jarratt on 1/23/17.
+//  Copyright © 2017 General Linear Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fake4SwiftMatchers.
+FOUNDATION_EXPORT double Fake4SwiftMatchersVersionNumber;
+
+//! Project version string for Fake4SwiftMatchers.
+FOUNDATION_EXPORT const unsigned char Fake4SwiftMatchersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fake4SwiftMatchers/PublicHeader.h>
+
+
